78b482bafb32b9e68d59c8317dc336a229e1e0c5
Marcio Dutra <professormarciodutra@gmail.com>
2026-07-31 12:03:16 -0300

    chore: configure Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 function App() {
   return (
-    <h1 className="text-5xl font-bold text-blue-500">
-      Portfolio
-    </h1>
+    <div className="min-h-screen bg-slate-900 flex items-center justify-center">
+      <h1 className="text-6xl font-bold text-blue-500">
+        Meu Portfolio
+      </h1>
+    </div>
   );
 }
 
